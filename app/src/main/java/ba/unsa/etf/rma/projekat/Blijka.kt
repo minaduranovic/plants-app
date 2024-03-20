af760d701882ed7e29e910e0d1d22c6ed2e3939f
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-03-20 20:54:04 +0100

    medicinski item?

---
+++ app/src/main/java/ba/unsa/etf/rma/projekat/Blijka.kt
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rma.projekat
+
+class Blijka (
+   val naziv : String,
+    val porodica : String,
+    val  medicinskoUpozorenje : String,
+    val medicinskeKoristi : List<MedicinskaKorist>,
+    val profilOkusa : ProfilOkusaBiljke,
+    val jela : List<String>,
+    val klimatskiTipovi : List<KlimatskiTip>,
+    val zemljisniTipovi : List<Zemljiste>,
+)
--- app/src/main/java/ba/unsa/etf/rma/projekat/MainActivity.kt
+++ app/src/main/java/ba/unsa/etf/rma/projekat/MainActivity.kt
@@ -1,20 +1,33 @@
 package ba.unsa.etf.rma.projekat
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val spinner: Spinner = findViewById(R.id.modSpinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.modSpinner,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
         }
+
     }
+
 }
