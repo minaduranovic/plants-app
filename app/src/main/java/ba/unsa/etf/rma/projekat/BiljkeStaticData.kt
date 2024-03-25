dd9994a510bd715e27680726cb3fe6314f3659b4
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-03-25 12:15:54 +0100

    dodato jos biljaka

--- app/src/main/java/ba/unsa/etf/rma/projekat/BiljkeStaticData.kt
+++ app/src/main/java/ba/unsa/etf/rma/projekat/BiljkeStaticData.kt
@@ -18,29 +18,34 @@ val biljke = listOf(
         naziv = "Nana (Mentha spicata)",
         porodica = "Lamiaceae (metvice)",
         medicinskoUpozorenje = "Nije preporučljivo za trudnice, dojilje i djecu mlađu od 3 godine .",
-        medicinskeKoristi = listOf (MedicinskaKorist.PROTUUPALNO, MedicinskaKorist.PROTIVBOLOVA),
+        medicinskeKoristi = listOf(MedicinskaKorist.PROTUUPALNO, MedicinskaKorist.PROTIVBOLOVA),
         profilOkusa = ProfilOkusaBiljke.MENTA,
         jela = listOf("Jogurt sa voćem", "Gulaš"),
         klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.UMJERENA),
         zemljisniTipovi = listOf(Zemljiste.GLINENO, Zemljiste.CRNICA)
-   ),
+    ),
     Biljka(
-        naziv = "Kamilica (Matricaria chamomilla)",porodica = "Asteraceae (glavočike)",
+        naziv = "Kamilica (Matricaria chamomilla)", porodica = "Asteraceae (glavočike)",
         medicinskoUpozorenje = "Može uzrokovati alergijske reakcije kod osjetljivih osoba.",
-        medicinskeKoristi = listOf(MedicinskaKorist.SMIRENJE,
-        MedicinskaKorist.PROTUUPALNO),
+        medicinskeKoristi = listOf(
+            MedicinskaKorist.SMIRENJE,
+            MedicinskaKorist.PROTUUPALNO
+        ),
         profilOkusa = ProfilOkusaBiljke.AROMATICNO,
         jela = listOf("Čaj od kamilice"),
         klimatskiTipovi = listOf(KlimatskiTip.UMJERENA, KlimatskiTip.SUBTROPSKA),
         zemljisniTipovi = listOf(Zemljiste.PJESKOVITO, Zemljiste.KRECNJACKO)
-   ),
+    ),
     Biljka(
         naziv = "Ružmarin (Rosmarinus officinalis)",
         porodica = "Lamiaceae (metvice)",
         medicinskoUpozorenje = "Treba ga koristiti umjereno i konsultovati se sa ljekarom pri dugotrajnoj upotrebi ili upotrebi u većim količinama.",
-        medicinskeKoristi = listOf(MedicinskaKorist.PROTUUPALNO, MedicinskaKorist.REGULACIJAPRITISKA),
+        medicinskeKoristi = listOf(
+            MedicinskaKorist.PROTUUPALNO,
+            MedicinskaKorist.REGULACIJAPRITISKA
+        ),
         profilOkusa = ProfilOkusaBiljke.AROMATICNO,
-        jela = listOf("Pečeno pile", "Grah","Gulaš"),
+        jela = listOf("Pečeno pile", "Grah", "Gulaš"),
         klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.SUHA),
         zemljisniTipovi = listOf(Zemljiste.SLJUNKOVITO, Zemljiste.KRECNJACKO)
     ),
@@ -53,9 +58,64 @@ val biljke = listOf(
         jela = listOf("Jogurt sa voćem"),
         klimatskiTipovi = listOf(KlimatskiTip.SREDOZEMNA, KlimatskiTip.SUHA),
         zemljisniTipovi = listOf(Zemljiste.PJESKOVITO, Zemljiste.KRECNJACKO)
-)
+    ),
+    Biljka(
+        naziv = "Peršun (Petroselinum crispum)",
+        porodica = "Apiaceae (štitarka)",
+        medicinskoUpozorenje = "Osobe koje uzimaju lijekove za razrjeđivanje krvi trebaju izbjegavati konzumaciju peršuna u velikim količinama.",
+        medicinskeKoristi = listOf(
+            MedicinskaKorist.PODRSKAIMUNITETU,
+            MedicinskaKorist.PROTIVBOLOVA
+        ),
+        profilOkusa = ProfilOkusaBiljke.AROMATICNO,
+        jela = listOf("Čaj od peršuna", "Supa sa peršunom"),
+        klimatskiTipovi = listOf(KlimatskiTip.UMJERENA, KlimatskiTip.SUHA),
+        zemljisniTipovi = listOf(Zemljiste.ILOVACA, Zemljiste.KRECNJACKO)
+    ),
+    Biljka(
+        naziv = "Majčina dušica (Thymus vulgaris)",
+        porodica = "Lamiaceae (metvice)",
+        medicinskoUpozorenje = "Osobe koje pate od bolesti štitnjače trebaju izbjegavati konzumaciju velikih količina majčine dušice.",
+        medicinskeKoristi = listOf(MedicinskaKorist.SMIRENJE, MedicinskaKorist.PROTUUPALNO),
+        profilOkusa = ProfilOkusaBiljke.AROMATICNO,
+        jela = listOf("Čaj od majčine dušice", "Povrće sa začinima"),
+        klimatskiTipovi = listOf(KlimatskiTip.UMJERENA, KlimatskiTip.SUHA),
+        zemljisniTipovi = listOf(Zemljiste.KRECNJACKO, Zemljiste.PJESKOVITO)
+    ),
+    Biljka(
+        naziv = "Origano (Origanum vulgare)",
+        porodica = "Lamiaceae (metvice)",
+        medicinskoUpozorenje = "Osobe koje uzimaju lijekove za razrjeđivanje krvi trebaju izbjegavati konzumaciju origana u velikim količinama.",
+        medicinskeKoristi = listOf(MedicinskaKorist.PROTUUPALNO),
+        profilOkusa = ProfilOkusaBiljke.AROMATICNO,
+        jela = listOf("Pica", "Piletina s origanom"),
+        klimatskiTipovi = listOf(KlimatskiTip.UMJERENA, KlimatskiTip.SUHA),
+        zemljisniTipovi = listOf(Zemljiste.KRECNJACKO, Zemljiste.PJESKOVITO)
+    ),
+    Biljka(
+        naziv = "Kopriva (Urtica dioica)",
+        porodica = "Urticaceae (grmovi)",
+        medicinskoUpozorenje = "Može izazvati iritaciju kože kod dodira s dlakama na biljci.",
+        medicinskeKoristi = listOf(MedicinskaKorist.PODRSKAIMUNITETU, MedicinskaKorist.PROTUUPALNO),
+        profilOkusa = ProfilOkusaBiljke.GORKO,
+        jela = listOf("Pita od koprive"),
+        klimatskiTipovi = listOf(KlimatskiTip.UMJERENA, KlimatskiTip.SUHA),
+        zemljisniTipovi = listOf(Zemljiste.GLINENO)
+    ),
+    Biljka(
+        naziv = "Šafran (Crocus sativus)",
+        porodica = "Iridaceae (perunike)",
+        medicinskoUpozorenje = "Prekomjerna konzumacija može izazvati nuspojave kod nekih ljudi.",
+        medicinskeKoristi = listOf(MedicinskaKorist.SMIRENJE, MedicinskaKorist.PROTUUPALNO),
+        profilOkusa = ProfilOkusaBiljke.GORKO,
+        jela = listOf("Rižoto od šafrana","Čaj od šafrana" ),
+        klimatskiTipovi = listOf(KlimatskiTip.UMJERENA, KlimatskiTip.SUHA),
+        zemljisniTipovi = listOf(Zemljiste.PJESKOVITO)
+    )
+
+
 )
 
 fun getBiljkeList(): List<Biljka> {
-return biljke;
+    return biljke;
 }
